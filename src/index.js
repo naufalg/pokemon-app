00cbb56b98e1679dfa9ae30be4b79a3e31135327
naufalg <naufalghifari7@gmail.com>
2021-03-24 18:06:42 +0700

    add service worker

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import swDev from "./swDev";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
+swDev();
 reportWebVitals();
